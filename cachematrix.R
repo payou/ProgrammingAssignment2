29b504809e44a9c33441df8a2b1ae144359da5ee
oandre <oandre@amfinesoft.com>
2020-06-26 17:11:49 +0200

    Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,11 @@
 ## Programmig assignment 2
+## Try it :
+## x <- matrix(c(1,3,5,4), nrow=2, ncol= 2)
+## x
+## solve(x)
+## m <- makeCacheMatrix(x)
+## cacheSolve(m)
+## cacheSolve(m)
 
 ## Special matrix. Get/set the matrix. Get/set the inverse matrix.
 
